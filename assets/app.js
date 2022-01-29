e6979d643e6116bc4f8f7c0b200d0beb570360b4
greg <greg@drdata.io>
2022-01-29 18:12:05 +0100

    first commit

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import './styles/app.scss';
+
+// import bootstrap for js
+import 'bootstrap';
+// require jQuery normally
+require('jquery');
+// start the Stimulus application
+import './bootstrap';

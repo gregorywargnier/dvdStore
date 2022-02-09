ed10711f7467b82dd8c0b16d06dcba117ba25585
greg <greg@drdata.io>
2022-02-09 15:10:52 +0100

    category home

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class homeController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request, CategoryRepository $categoryRepo): Response
     {
-
-        return $this->render('home/index.html.twig', []);
+        $categories = $categoryRepo->findAll();
+        return $this->render('home/index.html.twig', [
+            'categories'=>$categories,
+        ]);
     }
 }

17dbe9581db2b78d77cd15cc61937e23fa50b49e
greg <greg@drdata.io>
2022-02-25 15:13:57 +0100

    translation

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -114,7 +114,7 @@ class RegistrationFormType extends AbstractType
                 ]
             ])
 
-
+            //password
             ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
